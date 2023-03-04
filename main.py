16102faa06cfa843ef56e789cfc93dc6e485322d
Muhammad Ibrahim <hama.s.ibrahim9@gmail.com>
2023-03-04 23:40:04 +0300

    basha kaka

--- main.py
+++ main.py
@@ -1,27 +1,16 @@
 import math
 import pygame
 import numpy
-import cell
 
 # Setup
-pygame.init()
 window_height = 630
 window_width = 1080
 cell_size = 18
 cols = math.ceil(window_width / cell_size)
 rows = math.ceil(window_height / cell_size)
-# cells_grid = numpy.zeros((cols, rows))
-cells_grid = []
-# updated_grid_cells = numpy.zeros((cols, rows))
-updated_grid_cells = []
+cells_grid = numpy.zeros((cols, rows))
 screen = pygame.display.set_mode((window_width, window_height))
 pygame.display.set_caption("Game of Life")
-
-for i in range(rows):
-    sub_arr = []
-    for j in range(cols):
-        sub_arr.append(cell.Cell(i, j))
-    cells_grid.append(sub_arr)
 
 # Colors
 BLACK = (0, 0, 0)
@@ -31,55 +20,61 @@ DARK_BLUE = (36, 73, 89)
 
 
 # Functions
+def draw(screen, row, col, color):
+    rect = pygame.Rect(row * cell_size, col * cell_size, cell_size - 1, cell_size - 1)
+    pygame.draw.rect(screen, color, rect, border_radius=2)
 
 
-def next_generation(sum_alive_neighbors, row, col):
+def next_generation(sum_alive_neighbors, row, col, updated_cells):
     cell = cells_grid[row][col]
+    cell_color = WHITE if cell == 0 else GRAY
     if cell == 1:
         if 2 == sum_alive_neighbors == 3:
-            cell.status = 1
+            cells_grid[row][col] = 1
+            cell_color = WHITE
         elif sum_alive_neighbors > 3 or sum_alive_neighbors < 2:
-            cell.status = 0
+            cells_grid[row][col] = 0
+            cell_color = GRAY
     else:
         if sum_alive_neighbors == 3:
-            cell.status = 1
+            updated_cells[row][col] = 1
+            cell_color = WHITE
         else:
-            cell.status = 0
+            cells_grid[row][col] = 0
+            cell_color = GRAY
 
-    cell_color = WHITE if cell.status == 0 else GRAY
+    draw(screen, row, col, cell_color)
+    # return updated_cells
 
 
-def count_alive_neighbors(row, col):
-    count = 0
-    for i in range(row - 1, row + 2):
-        for j in range(col - 1, col + 2):
-            if i == row and j == col:
-                continue
-            if 0 <= i < len(cells_grid) and 0 <= j < len(cells_grid[0]):
-                if cells_grid[i][j] == 1:
-                    count += 1
-    return count
+def count_alive_neighbors(row, col, cells):
+    print(row, col)
+    sum_alive_neighbors = numpy.sum(cells[row - 1:row + 2, col - 1: col + 2]) - cells[row][col]
+    return sum_alive_neighbors
 
 
 def update(cells):
-    for r in range(rows):
-        for c in range(cols):
-            sum_neighbors = count_alive_neighbors(r, c)
-            next_generation(sum_neighbors, r, c)
+    updated_grid_cells = numpy.zeros((cols, rows))
+    for r, c in numpy.ndindex(cells.shape):
+        sum_neighbors = count_alive_neighbors(r, c, cells)
+        next_generation(sum_neighbors, r, c, updated_grid_cells)
 
 
 def main():
-    cells_grid[8][9].status = 1
-    cells_grid[8][8].status = 1
-    cells_grid[8][7].status = 1
-    cells_grid[7][8].status = 1
-    cells_grid[7][9].status = 1
-    cells_grid[7][7].status = 1
-    cells_grid[6][8].status = 1
-    cells_grid[6][7].status = 1
-    cells_grid[6][9].status = 1
+    pygame.init()
+    cells_grid[8][9] = 1
+    cells_grid[8][8] = 1
+    cells_grid[8][7] = 1
+    cells_grid[7][8] = 1
+    cells_grid[7][9] = 1
+    cells_grid[7][7] = 1
+    cells_grid[6][8] = 1
+    cells_grid[6][7] = 1
+    cells_grid[6][9] = 1
 
     update(cells_grid)
+    pygame.display.update()
+    pygame.display.flip()
 
     done = False
     while not done:
@@ -93,22 +88,9 @@ def main():
                 if event.buttons[0]:
                     a = x // cell_size
                     b = y // cell_size
-                    cells_grid[a][b].status = 1
-                    # update(cells_grid)
-
-        for x in range(rows):
-            for y in range(cols):
-                cell = cells_grid[x][y]
-                if cell.status == 1:
-                    cell.draw(screen, cell_size, WHITE)
-
-                if cell.status == 0:
-                    cell.draw(screen, cell_size, GRAY)
-
-        screen.fill(BLACK)
-        pygame.display.flip()
-        # else:
-        #     update(cells_grid)
+                    cells_grid[a][b] = 1
+                    update(cells_grid)
+                    pygame.display.update()
 
 
 main()
