ce05e2860445c90c62ebe268f4b126c26b9ecded
Muhammad Ibrahim <hama.s.ibrahim9@gmail.com>
2023-03-05 00:48:39 +0300

    90 percent done

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ BLACK = (0, 0, 0)
 GRAY = (33, 37, 41)
 WHITE = (255, 255, 255)
 DARK_BLUE = (36, 73, 89)
+WHITE_TINT = (160, 160, 160)
 
 
 # Functions
@@ -27,37 +28,37 @@ def draw(screen, row, col, color):
     pygame.draw.rect(screen, color, rect, border_radius=2)
 
 
-def next_generation(sum_alive_neighbors, row, col, updated_cells):
+def next_generation(sum_alive_neighbors, row, col, updated_cells, next):
     cell = cells_grid[row, col]
     cell_color = WHITE if cell == 1 else GRAY
     if cell == 1:
         if sum_alive_neighbors == 3 or sum_alive_neighbors == 2:
             updated_cells[row, col] = 1
-            # cell_color = WHITE
+            if next:
+                cell_color = WHITE
         elif sum_alive_neighbors > 3 or sum_alive_neighbors < 2:
-            cell_color = GRAY
+            if next:
+                cell_color = GRAY
     else:
         if sum_alive_neighbors == 3:
             updated_cells[row, col] = 1
-            # cell_color = WHITE
-        # else:
-        #     cell_color = GRAY
+            if next:
+                cell_color = WHITE
 
     draw(screen, row, col, cell_color)
     return updated_cells
 
 
 def count_alive_neighbors(row, col, cells):
-    print(row, col)
     sum_alive_neighbors = numpy.sum(cells[row - 1:row + 2, col - 1: col + 2]) - cells[row][col]
     return sum_alive_neighbors
 
 
-def update(cells):
+def update(cells, next=False):
     updated_grid_cells = numpy.zeros((cols, rows))
     for r, c in numpy.ndindex(cells.shape):
         sum_neighbors = count_alive_neighbors(r, c, cells)
-        updated_grid_cells = next_generation(sum_neighbors, r, c, updated_grid_cells)
+        updated_grid_cells = next_generation(sum_neighbors, r, c, updated_grid_cells, next)
 
     return updated_grid_cells
 
@@ -85,18 +86,18 @@ def start(cells_grid):
                         cells_grid[a, b] = 1
                         update(cells_grid)
                         pygame.display.update()
-            elif event.type == pygame.KEYDOWN:
+            elif event.type == pygame.KEYDOWN and not event.type == pygame.MOUSEMOTION:
                 if event.key == pygame.K_SPACE:
                     start = not start
-                    update(cells_grid)
+                    cells_grid = update(cells_grid, False)
                     pygame.display.update()
 
         screen.fill(BLACK)
         if start:
-            cells_grid = update(cells_grid)
+            cells_grid = update(cells_grid, True)
             pygame.display.update()
 
-        time.sleep(0.0001)
+        time.sleep(0.01)
 
 
 if __name__ == "__main__":
